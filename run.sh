0845442ed3579e60dc9b8a6bd0a868d63866d293
Aaron Conole <aconole@redhat.com>
2018-11-21 10:59:17 -0500

    send to a flow table
    Signed-off-by: Aaron Conole <aconole@redhat.com>

--- run.sh
+++ run.sh
@@ -17,6 +17,10 @@ sudo modprobe nft_queue
 sudo modprobe nft_chain_nat_ipv4
 sudo modprobe nft_chain_route_ipv4
 sudo modprobe nft_chain_fib_ipv4
+sudo modprobe nft_masq
+sudo modprobe nft_masq_ipv4
+sudo modprobe nft_redir
+sudo modprobe nft_redir_ipv4
 
 if [ "X$tablename" == "X" ]; then
     tablename="t"
@@ -40,12 +44,19 @@ sudo ~/git/nftables/src/nft flush ruleset
 sudo ~/git/nftables/src/nft add table "$tablename"
 sudo ~/git/nftables/src/nft add flowtable "$tablename" "$flowname" { hook ingress priority 10\; devices = { "$device" }\; }
 
-sudo ~/git/nftables/src/nft add chain "$tablename" "$chainname" { type filter hook input priority 0\; policy accept\; }
+sudo ~/git/nftables/src/nft add chain "$tablename" "$chainname" { type filter hook forward priority 0\; policy accept\; }
+
+sudo ~/git/nftables/src/nft add chain "$tablename" "natin_${chainname}" { type nat hook prerouting priority 0 \; }
+sudo ~/git/nftables/src/nft add chain "$tablename" "natout_${chainname}" { type nat hook postrouting priority 100 \; }
+sudo ~/git/nftables/src/nft add rule "$tablename" "natout_${chainname}" ip saddr 192.168.122.0/24 oifname "$internet" masquerade
+sudo ~/git/nftables/src/nft add rule "$tablename" "natout_${chainname}" counter
+
 
 sudo ~/git/nftables/src/nft add chain "$tablename" "off_${chainname}"
 sudo ~/git/nftables/src/nft add rule "$tablename" "off_$chainname" flow offload "@$flowname"
 
 sudo ~/git/nftables/src/nft add rule "$tablename" "$chainname" ip protocol tcp jump "off_$chainname"
+sudo ~/git/nftables/src/nft add rule "$tablename" "$chainname" ip protocol icmp jump "off_$chainname"
 sudo ~/git/nftables/src/nft add rule "$tablename" "$chainname" counter
 
 sudo ~/git/nftables/src/nft list ruleset
